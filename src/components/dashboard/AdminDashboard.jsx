a66b0dabc863d8ffc47ba9a7d1458a93dd75e9ef
Nisha BI <nishaturkey7500@gmail.com>
2026-02-18 21:48:17 +0530

    import location change

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../header/header";
+import Header from "../header/Header";
 import CreateTask from "../header/CreateTask";
 import AllTask from "../header/AllTask";
 
